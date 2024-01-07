a4cfff314182a3047b5c0b0be5d3906e805c3b46
Kwanwoo Kim <devkwanwoo@gmail.com>
2024-01-07 16:51:03 +0900

    Create 24883.cpp

---
+++ C++/2023-1/24883.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (void){
+    char a;
+    cin >> a;
+    if(a == 'N' || a=='n'){
+        cout << "Naver D2";
+    }else{
+        cout << "Naver Whale";
+    }
+}
