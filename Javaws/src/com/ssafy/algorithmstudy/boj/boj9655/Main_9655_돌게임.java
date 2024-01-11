d57177290553d5955ef4a596336c5eda4c5f19ef
gwanu-dev <devkwanwoo@gmail.com>
2024-01-11 23:24:52 +0900

    [BOJ] 2410111 solve boj9655

---
+++ Javaws/src/com/ssafy/algorithmstudy/boj/boj9655/Main_9655_돌게임.java
@@ -0,0 +1,17 @@
+package com.ssafy.algorithmstudy.boj.boj9655;
+
+import java.util.*;
+
+public class Main_9655_돌게임 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if(a < 4){
+            System.out.println("SK");
+        }else{
+            if(a % 2 == 0) {System.out.println("CY");}
+            else{ System.out.println("SK");}
+        }
+    }
+}
