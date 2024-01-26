d8c5b8472946eaa7e5ca11b692ba3c98e427565e
Kwanwoo Kim <devkwanwoo@gmail.com>
2024-01-26 22:56:43 +0900

    Create boj14645.java

---
+++ Javaws/src/com/ssafy/algorithmstudy/boj14645.java
@@ -0,0 +1,18 @@
+// import java.util.*;
+// import java.io.*;
+
+// public class Main{
+//     public static void main(String args[]) throws IOException{
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         int N, M;
+//         StringTokenizer st = new StringTokenizer(br.readLine());
+        
+//         N = Integer.parseInt(st.nextToken());
+//         M = Integer.parseInt(st.nextToken());
+        
+//         for(int k = 0 ; k < N ; k ++){
+//             String str = br.readLine();
+//         }
+//         System.out.println("비와이");
+//     }
+// }
