8d97d01e1d68d12e02eea4d1ce403fb955a3f7f0
Kwanwoo Kim <devkwanwoo@gmail.com>
2024-01-28 22:07:38 +0900

    Create boj20492.java

---
+++ Javaws/src/com/ssafy/algorithmstudy/boj/boj20492.java
@@ -0,0 +1,10 @@
+// import java.util.*;
+
+// public class Main{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+        
+//         int s = sc.nextInt();
+//         System.out.println(((int)(s * 0.78)) + " " + ((int)(s * 0.956)));
+//     }
+// }
