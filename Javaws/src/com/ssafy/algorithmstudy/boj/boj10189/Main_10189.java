0c9e9acb70d8c700c46e72cde1c5d3b01c5fe383
gwanu-dev <devkwanwoo@gmail.com>
2024-01-31 22:34:19 +0900

    [BOJ] 240130 solve boj10189

---
+++ Javaws/src/com/ssafy/algorithmstudy/boj/boj10189/Main_10189.java
@@ -0,0 +1,11 @@
+package com.ssafy.algorithmstudy.boj.boj10189;
+
+
+public class Main_10189 {
+    public static void main(String args[]){
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
