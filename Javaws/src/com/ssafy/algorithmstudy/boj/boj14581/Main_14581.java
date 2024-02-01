a8a4a0ca834aae6924508ae82813f5007712527b
gwanu-dev <devkwanwoo@gmail.com>
2024-02-01 23:33:54 +0900

    [BOJ] 240201 solve boj14581

---
+++ Javaws/src/com/ssafy/algorithmstudy/boj/boj14581/Main_14581.java
@@ -0,0 +1,17 @@
+package com.ssafy.algorithmstudy.boj.boj14581;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_14581 {
+    public static void main(String[] args) throws IOException {
+        String st = br.readLine();
+        System.out.println(":fan::fan::fan:\n" +
+                ":fan::" + st + "::fan:\n" +
+                ":fan::fan::fan:");
+    }
+
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+}
