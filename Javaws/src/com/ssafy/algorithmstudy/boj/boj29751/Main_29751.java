4f866943bca6a3557c650867483ef74ac9f7ed7e
gwanu-dev <devkwanwoo@gmail.com>
2024-02-15 23:30:19 +0900

    [BOJ] 240215 solve boj29751

---
+++ Javaws/src/com/ssafy/algorithmstudy/boj/boj29751/Main_29751.java
@@ -0,0 +1,12 @@
+package com.ssafy.algorithmstudy.boj.boj29751;
+
+import java.util.Scanner;
+
+public class Main_29751 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%.1f", ((float)(a * b)) / 2);
+    }
+}
