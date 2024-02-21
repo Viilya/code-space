de3b51d642d162764a428ae2c33541e3d971c251
Kwanwoo Kim <devkwanwoo@gmail.com>
2024-02-21 21:27:44 +0900

    Create  15963.java

---
+++ Javaws/src/com/ssafy/algorithmstudy/15963.java
@@ -0,0 +1,17 @@
+
+// import java.util.*;
+
+// public class Main
+// {
+// 	public static void main (String[] args) throws java.lang.Exception
+// 	{
+// 		Scanner sc = new Scanner(System.in);
+// 		long a = sc.nextLong();
+// 		long b = sc.nextLong();
+// 		if(a == b){
+// 			System.out.println("1");
+// 		}else{
+// 			System.out.println("0");
+// 		}
+// 	}
+// }
