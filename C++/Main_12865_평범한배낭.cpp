f564a9cf26a3a01e17517d0eeeec8a7da95b15ff
gwanu-dev <devkwanwoo@gmail.com>
2024-02-23 10:56:14 +0900

    [BOJ] 240223 solving boj12865

--- C++/Main_12865_평범한배낭.cpp
+++ C++/Main_12865_평범한배낭.cpp
@@ -8,20 +8,26 @@
  * 가치의 최댓값
  */
 
-
 #include <iostream>
 #include <queue>
-
+#include <math.h>
 #define FIO cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
 using namespace std;
 
+class Stuff
+{
+public:
+    int index;
 
-class Stuff{
-    public: int index;
-    public: int weight;
-    public: int value;
+public:
+    int weight;
 
-    public: Stuff(int i, int w, int v){
+public:
+    int value;
+
+public:
+    Stuff(int i, int v, int w)
+    {
         this->index = i;
         this->weight = w;
         this->value = v;
@@ -30,58 +36,79 @@ class Stuff{
 
 int N, K;
 int stuff[100][2]; // 0 : val , 1 : wgt
-int dp[100000];
+int dp[100001];
 
-void input(){
-    cin >> N >> K ;
-    for(int k = 0 ; k < N ; k ++){
+void input()
+{
+    cin >> N >> K;
+
+    for (int k = 0; k < 100001; k++)
+    {
+        dp[k] = 0;
+    }
+    for (int k = 0; k < N; k++)
+    {
         int wgt, val;
         cin >> wgt >> val;
         stuff[k][0] = val;
         stuff[k][1] = wgt;
-    }
-
-    for(int k = 0 ; k < K+1; k++){
-        dp[k] = 0;
+        dp[wgt] = max(dp[wgt], val);
     }
 }
 
-void solve() {
+void solve()
+{
     queue<Stuff> v;
-    for(int k = 0 ; k < N ; k ++){
-        Stuff initStuff = Stuff(k, stuff[k][0], stuff[k][1]);
-        v.push(initStuff);
+    for (int k = 0; k < N; k++)
+    {
 
-        while(!v.empty()){
+        v.push(Stuff(k, stuff[k][0], stuff[k][1]));
+
+        while (!v.empty())
+        {
+
             Stuff currStuff = v.front();
             v.pop();
 
             int currStuffNumber = currStuff.index;
             int currSumWeight = currStuff.weight;
             int currSumValue = currStuff.value;
-
-            for(int s = currStuffNumber + 1 ;s < N ; s++){
+            // cout << currStuffNumber << " " << currSumWeight << " " << currSumValue << "\n";
+            for (int s = currStuffNumber + 1; s < N; s++)
+            {
 
                 int sum = currSumWeight + stuff[s][1];
-                if(currSumWeight + stuff[s][1] > K) continue;
+                // cout << sum << " \n";
+                if (currSumWeight + stuff[s][1] > K)
+                    continue;
+
                 int val = currSumValue + stuff[s][0];
-                if(dp[sum] > val){
-
+                // cout << val << " \n";
+                if (dp[sum] < val)
+                {
+                    dp[sum] = val;
+                    v.push(Stuff(s, val, sum));
                 }
-
             }
         }
     }
 }
 
+void output()
+{
+    int maxVal = 0;
+    for (int k = 0; k < K + 1; k++)
+    {
+        maxVal = max(maxVal, dp[k]);
+    }
 
+    cout << maxVal << "\n";
+}
 
-
-
-
-
-int main(void){
+int main(void)
+{
     FIO;
     input();
     solve();
+    output();
 }
